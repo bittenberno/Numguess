7b83c08dc2a51400cfb5e29cfba709ccb5958e00
bittenberno <46707471+bittenberno@users.noreply.github.com>
2020-05-30 21:24:08 +0200

    Add - Highscore list - mainmenu

--- Zahlenraten_v1.0.2/Zahlenraten_1_0_2.py
+++ Zahlenraten_v1.0.2/Zahlenraten_1_0_2.py
@@ -1,40 +1,49 @@
 import random
-#
-rannum = random.randrange(0, 100)
-#print("The random numer is", rannum)
-i=1
 
-choose=input('Drück "P" um zu spielen oder "H" um den Highscore anzuzeigen')
-if choose=="p":
-  while i==1:
-    for n in range(1,8):
-      print(str(n) + ". Versuch")
-      #def guesinput(n):
-      guesnum=int(input("Bitte eine Zahl zwischen 1 - 100 eingeben"))
-      if guesnum==rannum:
-        print("Herzlichen Glückwunsch du hast die Zahl beim " + str(n) + ". Versunch erraten.")
-        print("Die gesuchte Zahl ist", str(rannum))
-        name=input('Bitte gib deinen Namen eingeben.')
+#print("The random numer is", rannum)
+
+o=1
+
+
+while o==1:
+  choose=input('Press P to Play\nPress H to show Highscore\nPress E to exit')
+  if choose=="p": 
+    i=1                # IF choose Play or show Highscore
+    while i==1:                   # WHILE Play
+      rannum = random.randrange(0, 100)  
+      print("The random numer is", rannum)
+      for n in range(1,8):
+        print(str(n) + ". Versuch")
+        guesnum=int(input("Bitte eine Zahl zwischen 1 - 100 eingeben"))
+        if guesnum==rannum:       # IF Eraten
+          print("Herzlichen Glückwunsch du hast die Zahl beim " + str(n) + ". Versunch erraten.")
+          print("Die gesuchte Zahl ist", str(rannum))
+          name=input('Bitte gib deinen Namen eingeben.')
+          
+          f = open('Highscore', 'a')
+          f.write('\n' + name + ' -- ' + str(n) + ' Versuche')
+          break
+        elif guesnum>rannum:      # IF kleiner
+          print("Die Zahl ist kleiner als", str(guesnum))
+        elif guesnum<rannum:      # IF größer
+          print("Die Zahl ist größer als", str(guesnum))
+        else:
+          print("ungültige Eingabe")
         
-        f = open('completeName', 'a')
-        f.write('\n' + name + ' -- ' + str(n) + ' Versuche')
-        break
-      elif guesnum>rannum:
-        print("Die Zahl ist kleiner als", str(guesnum))
-      elif guesnum<rannum:
-        print("Die Zahl ist größer als", str(guesnum))
-      else:
-        print("ungültige Eingabe")
+        if n==7:                  # IF Game Over
+          print('Die gesuchte Zahl war' + str(rannum) +'.')
+          print("Game Over")
+          break
+      print("Danke fürs spielen.")
       
-      if n==7:
-        print('Die gesuchte Zahl war' + str(rannum) +'.')
-        print("Game Over")
-    
-    print("Danke fürs spielen.")
-    
-    again=input('Press "J" to play again. Press any other key to close the window.')
-    if again!="j":
-      break
-elif choose=="h":
- # print(Highscore)
-  input('Press any key to close the window.')
+      again=input('Press Enter to play again. Press any other key to close the window.')
+      if again!="":
+        break
+
+  elif choose=="h":
+    Highscore=open('Highscore', 'r')
+    print(Highscore.read())
+    chooseh=input('Press any key to go back to menu.')
+  
+  elif choose == 'e':
+    break
