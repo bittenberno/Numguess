1ffe3174e453ae007eb7042163acf1c81b4e37f8
bittenberno <berno3000@web.de>
2020-05-30 19:03:57 +0200

    1

--- Zahlenraten_v1.0.2/Zahlenraten_1_0_2.py
+++ Zahlenraten_v1.0.2/Zahlenraten_1_0_2.py
@@ -4,22 +4,22 @@ rannum = random.randrange(0, 100)
 i=1
 while i==1:
   for n in range(1,8):
-    print(str(n) +". Versuch")
+    print(str(n) + ". Versuch")
     #def guesinput(n):
     guesnum=int(input("Bitte eine Zahl zwischen 1 - 100 eingeben"))
-
     if guesnum==rannum:
-      print("Herzlichen Glückwunsch du hast die Zahl beim " + str(n) + ". Versunch erraten. **********")
-      print("Die gesuchte Zahl ist", rannum)
+      print("Herzlichen Glückwunsch du hast die Zahl beim " + str(n) + ". Versunch erraten.")
+      print("Die gesuchte Zahl ist", str(rannum))
       break
     elif guesnum>rannum:
-      print("Die Zahl ist kleiner als", guesnum)
+      print("Die Zahl ist kleiner als", str(guesnum))
     elif guesnum<rannum:
-      print("Die Zahl ist größer als", guesnum)
+      print("Die Zahl ist größer als", str(guesnum))
     else:
       print("ungültige Eingabe")
     
     if n==7:
+      print('Die gesuchte Zahl war' + str(rannum) +'.')
       print("Game Over")
   
   print("Danke fürs spielen.")
